fe74c86a7b610579ec1c8034a5d79ec7a83087e8
djordjenikolic960 <djordje.nikolic960@gmail.com>
2021-10-06 19:23:32 +0200

    - verzija 2.0

---
+++ app/src/main/java/com/example/mems/model/DareListItem.kt
@@ -0,0 +1,10 @@
+package com.example.mems.model
+
+data class DareListItem(
+    val id: String? = null,
+    val category: String? = null,
+    val title: String? = null,
+    val text: String? = null,
+    var score: Int? = null,
+    val whoDared: String? = null
+)
